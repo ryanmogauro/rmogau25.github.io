7f84183046ff24cc51d5b07abc69cc3c40bbf321
Ryan Mogauro <89362786+ryanmogauro@users.noreply.github.com>
2024-10-15 15:15:25 -0400

    committing rest of the project template

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Navigation Toggle
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+});
